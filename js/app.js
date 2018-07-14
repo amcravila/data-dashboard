a25256a7452e44ae9df9806d52352027ca9b9532
amcravila <amcravila@gmail.com>
2018-07-14 09:44:12 -0300

    Atualização 140718 0944

--- js/app.js
+++ js/app.js
@@ -1,6 +1,29 @@
-/*
- * Funcionalidad de tu producto
- */
+var subMenu = document.querySelectorAll('.subMenu');
+for(var i=0; i < subMenu.length; i++) {
+  subMenu[i].addEventListener('click', showMenu);
+}
 
-// Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+function showMenu() {
+ var itemMenu = this.getElementsByClassName('itemMenu')[0];
+ if (itemMenu.classList.contains('hide')) {
+   itemMenu.classList.remove('hide');
+   itemMenu.classList.add('show');
+ // } else {
+ //   itemMenu.classList.remove('show');
+ //   itemMenu.classList.add('hide');
+ // }
+}
+}
+//https://www.w3schools.com/howto/howto_js_dropdown.asp
+// function showMenu() {
+//   if (!event.target.matches('.subMenu')) {
+//
+//     var itemMenu = document.getElementsByClassName('itemMenu');
+//     for (var i = 0; i < itemMenu.length; i++) {
+//       var openDropdown = itemMenu[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
