245a60181c5cb2bf0991d46cb9e5cb5a39d6f5a1
amcravila <amcravila@gmail.com>
2018-07-15 13:20:25 -0300

    Menu Filtros 15072018 1320

--- js/app.js
+++ js/app.js
@@ -1,32 +1,36 @@
-// var subMenu = document.querySelectorAll('.subMenu');
-// for(var i=0; i < subMenu.length; i++) {
-//   subMenu[i].addEventListener('click', showMenu);
-//   subMenu[i].addEventListener('click', overview);
-// }
-//
-// function overview(){
-//   var sede = subMenu[i].value;
-//   console.log(sede);
-// }
+var hostCityAQP = document.getElementById('AQP');
+hostCityAQP.addEventListener('mouseover', selectedHostCity);
+var hostCityCDMX = document.getElementById('CDMX');
+hostCityCDMX.addEventListener('mouseover', selectedHostCity);
+var hostCityLIM = document.getElementById('LIM');
+hostCityLIM.addEventListener('mouseover', selectedHostCity);
+var hostCitySCL = document.getElementById('SCL');
+hostCitySCL.addEventListener('mouseover', selectedHostCity);
+
+function selectedHostCity() {
+  var selectedHostCity = document.getElementById('students').innerHTML = this.id;
+}
 
 var studentsClasses = document.querySelectorAll('.studentsClass');
 for(var i=0; i < studentsClasses.length; i++) {
-  studentsClasses[i].addEventListener('click', students);
-  studentsClasses[i].addEventListener('click', overview);
+  studentsClasses[i].addEventListener('click', selectedClass);
 }
 
-function students() {
-  console.log ('função estudantes OK');
+function selectedClass(e) {
+  var selectedClass = document.getElementById('students').innerHTML = this.textContent;
+  e.stopPropagation();
 }
 
-//código da NOSSASPROGRAMADORAS
-// var students = document.getElementById('students');
-// students.innerHTML = "";
-// for (turma in data[sede]) {
-//   for (i in data[sede][turma]["students"]) {
-//     var img = document.createElement("img");
-//     img.src = data[sede][turma]["students"][i]["photo"];
-//     students.appendChild(img);
+
+
+// function students() {
+//   var students = document.getElementById('students');
+//   students.innerHTML = "";
+//   for (turma in data[sede]) {
+//     for (i in data[sede][turma]["students"]) {
+//       var img = document.createElement("img");
+//       img.src = data[sede][turma]["students"][i]["photo"];
+//       students.appendChild(img);
+//     }
 //   }
-// }
 // }
