28f4936c4221d45fcd522d9bfbbabfde66c25b1e
Karine Coutinho <ka.coutinho@hotmail.com>
2018-07-15 16:29:20 -0300

    variavel sede/turma - 15072018 1630

--- js/app.js
+++ js/app.js
@@ -1,25 +1,32 @@
-var hostCityAQP = document.getElementById('AQP');
-hostCityAQP.addEventListener('mouseover', selectedHostCity);
-var hostCityCDMX = document.getElementById('CDMX');
-hostCityCDMX.addEventListener('mouseover', selectedHostCity);
-var hostCityLIM = document.getElementById('LIM');
-hostCityLIM.addEventListener('mouseover', selectedHostCity);
-var hostCitySCL = document.getElementById('SCL');
-hostCitySCL.addEventListener('mouseover', selectedHostCity);
+// var hostCityAQP = document.getElementById('AQP');
+// console.log(hostCityAQP);
+// hostCityAQP.addEventListener('mouseover', selectedHostCity);
+// var hostCityCDMX = document.getElementById('CDMX');
+// hostCityCDMX.addEventListener('mouseover', selectedHostCity);
+// var hostCityLIM = document.getElementById('LIM');
+// hostCityLIM.addEventListener('mouseover', selectedHostCity);
+// var hostCitySCL = document.getElementById('SCL');
+// hostCitySCL.addEventListener('mouseover', selectedHostCity);
 
+var hostCity = document.querySelectorAll('.hostCity');
+for(var i=0; i < hostCity.length; i++) {
+  hostCity[i].addEventListener('mouseover', selectedHostCity);
+}
 function selectedHostCity() {
-  var selectedHostCity = document.getElementById('selectedFilter').innerHTML = this.id;
+  var selectedHostCity = document.getElementById('selectedCity').innerHTML = this.id;
 }
 
 var studentsClasses = document.querySelectorAll('.studentsClass');
 for(var i=0; i < studentsClasses.length; i++) {
   studentsClasses[i].addEventListener('click', selectedClass);
 }
-
 function selectedClass(e) {
-  var selectedClass = document.getElementById('selectedFilter').innerHTML = this.textContent;
+  var selectedClass = document.getElementById('selectedClass').innerHTML = this.textContent;
   e.stopPropagation();
 }
+
+var selectedCity = document.getElementById('selectedCity');
+var selectedClass = document.getElementById('selectedClass');
 
 
 
