965c7b54d681bcbfeaae85fddef27d887a657892
amcravila <amcravila@gmail.com>
2018-07-16 17:21:30 -0300

    Tab Estudantes 160718 1721

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//função selecionar SEDE
 var hostCities = document.querySelectorAll('.hostCity');
 for(var i=0; i < hostCities.length; i++) {
   hostCities[i].addEventListener('mouseover', selectedHostCity);
@@ -9,10 +10,13 @@ function selectedHostCity(event) {
   var selectedCity = document.getElementById('filterCity').innerHTML = this.id;
 }
 
+
+//função selecionar TURMA
 var studentsClasses = document.querySelectorAll('.studentsClass');
 for(var i=0; i < studentsClasses.length; i++) {
   studentsClasses[i].addEventListener('click', selectedStudentClass);
   studentsClasses[i].addEventListener('click', panel);
+  // studentsClasses[i].addEventListener('mouseup', closeMenu);
 }
 
 var selectedClass = document.getElementById('filterClass');
@@ -21,7 +25,15 @@ function selectedStudentClass() {
   var selectedClass = document.getElementById('filterClass').innerHTML = this.textContent;
 }
 
+// function closeMenu(){
+//   var menuDrop = document.getElementsByClassName('dropDown');
+//   for(var i=0; i < menuDrop.length; i++) {
+//     menuDrop.setAttribute('id', 'hide');
+//   }
+// }
 
+
+//função exibir PAINE/ABAS
 function panel() {
   var panelOne = document.createElement('li');
   var tabOverview = document.createTextNode('OVERVIEW');
@@ -43,26 +55,46 @@ function panel() {
 }
 
 
+
+//função exibir dados na aba ESTUDANTES
 function panelStudents() {
   var city = selectedCity.textContent;
   var cityClass = selectedClass.textContent;
   var students = document.getElementById('students');
   students.innerHTML = '';
 
+//função estudantes ativas e inativas
+  var activeStudents = 0;
+  var desertionStudentsRate = 0;
+  var desertedStudents = 0;
+
+  for(var i=0; i<data[city][cityClass]['students'].length; i++){
+     if(data[city][cityClass]['students'][i].active === true ){
+       activeStudents++;
+     }
+     else{
+       desertedStudents++;
+     }
+   }
+
+
+
+//está duplicando, entrando no array RATINGS
   for (i in data[city][cityClass]) {
     for (j in data[city][cityClass]['students']) {
       var img = document.createElement('img');
       img.src = data[city][cityClass]['students'][j]['photo'];
       students.appendChild(img);
       var pName = document.createElement('p');
-      var studentName = document.createTextNode(data[city][cityClass]['students'][j]['name']);
-      pName.appendChild(studentName);
+      var studentName = document.createTextNode(data[city][cityClass]['students'][j]['name']);        pName.appendChild(studentName);
       students.appendChild(pName);
+      }
     }
-  }
 }
 
 
+
+//função exibir dados na aba OVERVIEW
 function panelOverview() {
   alert("clicou Overview");
 }
